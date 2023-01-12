daec4fafdb91bb100f69e897980726ae861f153e
JohnMFB <jjjaylij@gmail.com>
2023-01-12 18:41:29 -0500

    Fully Set Up Code, Ready For Programming 1.18.2

--- src/main/java/net/myfunnyblock/beginnings/Beginnings.java
+++ src/main/java/net/myfunnyblock/beginnings/Beginnings.java
@@ -17,24 +17,23 @@ import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(Beginnings.MOD_ID)
 public class Beginnings {
     public static final String MOD_ID = "beginnings";
 
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Beginnings() {
-        // Register the setup method for modloading
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
+
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
